5eb2d5b35402549d896ebe8600e5ff0eaa3c438b
Chibuike Okpaluba <chibuikepraise@gmail.com>
2019-09-22 14:38:06 +0100

    added basic error handling

---
+++ Simple Calculator/Simple Calculator/ArithmeticModule.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    public struct UnitProblemContextDescription {
+        public int head;
+        public int tail;
+        public string problem;
+        public string context;
+        public string solution;
+    };
+
+    public struct StringWordContextDescription
+    {
+        public int head;
+        public int tail;
+        public string sentence;
+        public string context;
+    };
+
+    class ArithmeticModule
+    {
+        public ArithmeticModule()
+        {
+        }
+
+        public string eval(string problem)
+        {
+            problem = "(" + problem + ")";
+            System.Diagnostics.Debug.WriteLine("Evaluating Problem: " + problem);
+
+            UnitProblemContextDescription context = getNextUnitProblemContextDescription(problem);
+
+            while (true)
+            {
+                // .... solve problem in context
+                System.Diagnostics.Debug.WriteLine("Evaluating New Context: " + context.context);
+                context.solution = solveContextProblem(context.context);
+
+                // .... replace solved context in problem with solution
+                context.problem = replaceSubString(context.problem, context.solution, context.head, context.tail);
+                context = getNextUnitProblemContextDescription(context.problem);
+
+                // .... if problem is solved
+                if (context.head == context.tail)
+                {
+                    if (context.problem == null)
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return context.problem;
+                    }
+                }
+            }
+        }
+
+        public string solveContextProblem(string contextProblem)
+        {
+            System.Diagnostics.Debug.WriteLine("Solving Context Problem: " + contextProblem);
+
+            // let's deal with (%) percentages first
+            contextProblem = solvePercentages(contextProblem);
+
+            // let's deal with (*) multiplications next
+            contextProblem = solveMultiplications(contextProblem);
+
+            // let's deal with (/) divisions next
+            contextProblem = solveDivisions(contextProblem);
+
+            // let's deal with (+) additions next
+            contextProblem = solveAdditions(contextProblem);
+
+            // let's deal with (-) subtractions next
+            contextProblem = solveSubtractions(contextProblem);
+
+            return contextProblem;
+        }
+
+        public string solvePercentages(string contextProblem)
+        {
+            while (contextProblem.Contains("%"))
+            {
+                System.Diagnostics.Debug.WriteLine("Solving Percentages for : " + contextProblem);
+
+                StringWordContextDescription contextDescription = getStringWordContextBeforeIndex(contextProblem, contextProblem.IndexOf('%'));
+                contextProblem = replaceSubString(contextProblem, percent(contextDescription.context), contextDescription.head, contextDescription.tail + 1);
+            }
+
+            return contextProblem;
+        }
+
+        public string solveMultiplications(string contextProblem)
+        {
+            while (contextProblem.Contains("*"))
+            {
+                System.Diagnostics.Debug.WriteLine("Solving Multiplication for : " + contextProblem);
+
+                StringWordContextDescription contextDescriptionA = getStringWordContextBeforeIndex(contextProblem, contextProblem.IndexOf('*')-1);
+                StringWordContextDescription contextDescriptionB = getStringWordContextAfterIndex(contextProblem, contextProblem.IndexOf('*') + 1);
+
+                contextProblem = replaceSubString(contextProblem, mul(contextDescriptionA.context, contextDescriptionB.context), contextDescriptionA.head, contextDescriptionB.tail);
+            }
+
+            return contextProblem;
+        }
+
+        public string solveDivisions(string contextProblem)
+        {
+            while (contextProblem.Contains("/"))
+            {
+                System.Diagnostics.Debug.WriteLine("Solving Divisions for : " + contextProblem);
+
+                StringWordContextDescription contextDescriptionA = getStringWordContextBeforeIndex(contextProblem, contextProblem.IndexOf('/') - 1);
+                StringWordContextDescription contextDescriptionB = getStringWordContextAfterIndex(contextProblem, contextProblem.IndexOf('/') + 1);
+
+                contextProblem = replaceSubString(contextProblem, div(contextDescriptionA.context, contextDescriptionB.context), contextDescriptionA.head, contextDescriptionB.tail);
+            }
+
+            return contextProblem;
+        }
+
+        public string solveAdditions(string contextProblem)
+        {
+            while (contextProblem.Contains("+"))
+            {
+                System.Diagnostics.Debug.WriteLine("Solving Additions for : " + contextProblem);
+
+                StringWordContextDescription contextDescriptionA = getStringWordContextBeforeIndex(contextProblem, contextProblem.IndexOf('+') - 1);
+                StringWordContextDescription contextDescriptionB = getStringWordContextAfterIndex(contextProblem, contextProblem.IndexOf('+') + 1);
+
+                contextProblem = replaceSubString(contextProblem, add(contextDescriptionA.context, contextDescriptionB.context), contextDescriptionA.head, contextDescriptionB.tail);
+            }
+
+            return contextProblem;
+        }
+
+        public string solveSubtractions(string contextProblem)
+        {
+            while (contextProblem.Contains("—"))
+            {
+                System.Diagnostics.Debug.WriteLine("Solving Subtractions for : " + contextProblem);
+
+                StringWordContextDescription contextDescriptionA = getStringWordContextBeforeIndex(contextProblem, contextProblem.IndexOf('—') - 1);
+                StringWordContextDescription contextDescriptionB = getStringWordContextAfterIndex(contextProblem, contextProblem.IndexOf('—') + 1);
+
+                contextProblem = replaceSubString(contextProblem, sub(contextDescriptionA.context, contextDescriptionB.context), contextDescriptionA.head, contextDescriptionB.tail);
+            }
+
+            //return mul(contextDescriptionA.context, contextDescriptionA.context) + contextDescriptionA.head.ToString() + ":" + contextDescriptionA.tail.ToString() + " vs " + contextDescriptionB.head.ToString() + ":" + contextDescriptionB.tail.ToString();
+            return contextProblem;
+        }
+
+        public StringWordContextDescription getStringWordContextBeforeIndex(string sentence, int endIndex)
+        {
+            StringWordContextDescription contextDescription = new StringWordContextDescription();
+            contextDescription.sentence = sentence;
+            contextDescription.tail = --endIndex;
+            contextDescription.head = endIndex;
+            contextDescription.context = "";
+
+            for (int i=endIndex; i>=0; i--)
+            {
+                char c = sentence.ElementAt(i);
+                if (!isNumber(c) && c != '.' && c != '-')
+                {
+                    break; // terminal character reached
+                }
+
+                contextDescription.head = i;
+            }
+
+            try
+            {
+                contextDescription.context = contextDescription.sentence.Substring(contextDescription.head, contextDescription.tail - contextDescription.head + 1);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                contextDescription.context = "ERROR";
+            }
+
+            return contextDescription;
+        }
+
+        public StringWordContextDescription getStringWordContextAfterIndex(string sentence, int startIndex)
+        {
+            StringWordContextDescription contextDescription = new StringWordContextDescription();
+            contextDescription.sentence = sentence;
+            contextDescription.tail = ++startIndex;
+            contextDescription.head = startIndex;
+            contextDescription.context = ":";
+
+            for (int i=startIndex; i < sentence.Length; i++)
+            {
+                char c = sentence.ElementAt(i);
+                if (!isNumber(c) && c != '.' && c != '-')
+                {
+                    break; // terminal character reached
+                }
+
+                contextDescription.tail = i;
+            }
+
+            try
+            {
+                contextDescription.context = contextDescription.sentence.Substring(contextDescription.head, contextDescription.tail - contextDescription.head + 1);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                contextDescription.context = "ERROR";
+            }
+
+            return contextDescription;
+        }
+
+        public string replaceSubString(string sentence, string subString, int head, int tail)
+        {
+            return sentence.Substring(0, head) + subString + sentence.Substring(tail + 1);
+        }
+
+        public UnitProblemContextDescription getNextUnitProblemContextDescription(string problem)
+        {
+            UnitProblemContextDescription context = new UnitProblemContextDescription();
+            context.problem = problem;
+
+            for (int i=0; i<problem.Length; i++)
+            {
+                switch (problem.ElementAt<char>(i))
+                {
+                    case '(':
+                        context.head = i;
+                        break;
+
+                    case ')':
+                        context.tail = i;
+                        context.context = problem.Substring(context.head + 1, context.tail - context.head - 1);
+                        return context;
+                }
+            }
+
+            return context;
+        }
+
+        public Boolean isNumber(string str)
+        {
+            try
+            {
+                int number = int.Parse(str);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        public Boolean isNumber(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+
+        // throws FormatException
+        public string add(string a, string b)
+        {
+            return (float.Parse(a) + float.Parse(b)).ToString();
+        }
+
+        public string sub(string a, string b)
+        {
+            return (float.Parse(a) - float.Parse(b)).ToString();
+        }
+
+        public string mul(string a, string b)
+        {
+            return (float.Parse(a) * float.Parse(b)).ToString();
+        }
+
+        public string div(string a, string b)
+        {
+            return (float.Parse(a) / float.Parse(b)).ToString();
+        }
+
+        public string percent(string a)
+        {
+            return (float.Parse(a) / 100.0).ToString();
+        }
+    }
+}
---
+++ Simple Calculator/Simple Calculator/MainPage.xaml.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Simple_Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        string solution = "";
+        string answer = "";
+        string numericBuffer = "";
+
+        int numberOfBracketsOpen = 0;
+        Boolean numberHasDot = false;
+        private ArithmeticModule stringEvaluator = new ArithmeticModule();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("7");
+        }
+
+        private void ButtonPressed(string buttonContent)
+        {
+            buttonContent = buttonContent.Trim();
+
+            if (stringEvaluator.isNumber(buttonContent))
+            {
+                numericBuffer += buttonContent;
+            }
+            else
+            {
+                if (buttonContent.Equals("CE"))
+                {
+                    answer = "";
+                    solution = "";
+                    numericBuffer = "";
+                    numberHasDot = false;
+                    numberOfBracketsOpen = 0;
+                }
+                else if (buttonContent.Equals("."))
+                {
+                    if ( numericBuffer.LastOrDefault<char>() != '.' && !numberHasDot)
+                    {
+                        if (numericBuffer.Length <= 0 || (numericBuffer.Length > 0 && !stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>())))
+                        {
+                            numericBuffer += "0";
+                        }
+
+                        if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()))
+                        {
+                            numericBuffer += buttonContent;
+                            numberHasDot = true;
+                        }
+                    }
+                }
+                else if (buttonContent.Equals("%"))
+                {
+                    if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()) || numericBuffer.LastOrDefault<char>() == ')')
+                    {
+                        numericBuffer += buttonContent;
+                    }
+                }
+                else if (buttonContent.Equals("+"))
+                {
+                    if (numericBuffer.Length > 0)
+                    {
+                        if (numberOfBracketsOpen <= 0)
+                        {
+                            solution += numericBuffer;
+                            solution += " + ";
+                            numericBuffer = "";
+                            numberHasDot = false;
+                        }
+                        else if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()) || numericBuffer.LastOrDefault<char>() == '%')
+                        {
+                            numericBuffer += " + ";
+                            numberHasDot = false;
+                        }
+                    }
+                }
+                else if (buttonContent.Equals("—"))
+                {
+                    if (numericBuffer.Length > 0)
+                    {
+                        if (numberOfBracketsOpen <= 0)
+                        {
+                            solution += numericBuffer;
+                            solution += " — ";
+                            numericBuffer = "";
+                            numberHasDot = false;
+                        }
+                        else if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()) || numericBuffer.LastOrDefault<char>() == '%')
+                        {
+                            numericBuffer += " — ";
+                            numberHasDot = false;
+                        }
+                    }
+                }
+                else if (buttonContent.Equals("/"))
+                {
+                    if (numericBuffer.Length > 0)
+                    {
+                        if (numberOfBracketsOpen <= 0)
+                        {
+                            solution += numericBuffer;
+                            solution += " / ";
+                            numericBuffer = "";
+                        }
+                        else if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()) || numericBuffer.LastOrDefault<char>() == '%')
+                        {
+                            numericBuffer += " / ";
+                        }
+
+                    }
+                }
+                else if (buttonContent.Equals("*"))
+                {
+                    if (numericBuffer.Length > 0)
+                    {
+                        if (numberOfBracketsOpen <= 0)
+                        {
+                            solution += numericBuffer;
+                            solution += " * ";
+                            numericBuffer = "";
+                            numberHasDot = false;
+                        }
+                        else if (stringEvaluator.isNumber(numericBuffer.LastOrDefault<char>()) || numericBuffer.LastOrDefault<char>() == '%')
+                        {
+                            numericBuffer += " * ";
+                            numberHasDot = false;
+                        }
+                    }
+                }
+                else if (buttonContent.Equals("("))
+                {
+                    numberOfBracketsOpen += 1;
+                    numericBuffer += buttonContent;
+                    numberHasDot = false;
+                }
+                else if (buttonContent.Equals(")") && numberOfBracketsOpen > 0)
+                {
+                    numberOfBracketsOpen = Math.Max(0, numberOfBracketsOpen - 1);
+                    numericBuffer += buttonContent;
+                }
+            }
+
+            
+            if (buttonContent.Equals("="))
+            {
+                try
+                {
+                    // evalute string and display solution
+                    solution += numericBuffer;
+                    numericBuffer = stringEvaluator.eval(solution);
+                    textBoxNumericView.Text = renderString(numericBuffer);
+                    textBoxSolutionView.Text = renderString(solution);
+                    textBoxNumericView.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(0x73, 0, 0, 0xC8));
+                    textBoxNumericView.Text = renderString(numericBuffer);
+                    textBoxSolutionView.Text = renderString(solution);
+
+                    // wipe solution ready for next task
+                    solution = "";
+                }
+                catch (Exception e)
+                {
+                    textBoxNumericView.Background = new SolidColorBrush(Windows.UI.Color.FromArgb(0x73, 0xC8, 0, 0));
+                    textBoxNumericView.Text = renderString(numericBuffer);
+                    textBoxSolutionView.Text = renderString(solution);
+                }
+            }
+            else
+            {
+                textBoxNumericView.Text = renderString(numericBuffer);
+                textBoxSolutionView.Text = renderString(solution);
+            }
+        }
+
+        private string renderString(string sentence)
+        {
+            return sentence.Replace('—', '-');
+        }
+
+        private void carryOver()
+        {
+
+        }
+
+        private void TextBoxNumericView_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("CE");
+        }
+
+        private void BtnEight_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("8");
+        }
+
+        private void BtnNine_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("9");
+        }
+
+        private void BtnFour_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("4");
+        }
+
+        private void BtnFive_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("5");
+        }
+
+        private void BtnSix_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("6");
+        }
+
+        private void BtnOne_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("1");
+        }
+
+        private void BtnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("2");
+        }
+
+        private void BtnThree_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("3");
+        }
+
+        private void BtnZero_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("0");
+        }
+
+        private void BtnDot_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed(".");
+        }
+
+        private void BtnPercentage_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("%");
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("+");
+        }
+
+        private void BtnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("—");
+        }
+
+        private void BtnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("*");
+        }
+
+        private void BtnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("/");
+        }
+
+        private void BtnOpenBracket_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("(");
+        }
+
+        private void BtnCloseBracket_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed(")");
+        }
+
+        private void BtnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPressed("=");
+        }
+    }
+}
